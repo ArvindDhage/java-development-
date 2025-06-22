d54ed994925a1fb61ad6a687121c51a4e7f664c1
Arvind Dhage <arvinddhage35@gmail.com>
2025-06-22 10:35:00 +0530

    Added LiteralsinJava.java

---
+++ untitled/src/LiteralsinJava.java
@@ -0,0 +1,16 @@
+public class LiteralsinJava {
+    public static void main(String[] args){
+        byte age=10;
+        int age2=30;
+        short s1=10;
+        String s2= "arvind";
+        float f1=20.30f;
+        long l1=2566666666l;
+        char ch='A';
+        double d1=20.4;
+        boolean a=true;
+        System.out.println(age);
+        System.out.println(s2);
+
+    }
+}
