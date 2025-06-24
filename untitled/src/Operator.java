1158a10c28970f81a9d931c398f86513d0937624
Arvind Dhage <arvinddhage35@gmail.com>
2025-06-24 22:07:32 +0530

    Comparison ,Assignment,logical,Bitwise

--- untitled/src/Operator.java
+++ untitled/src/Operator.java
@@ -20,9 +20,52 @@
     }*/
 
 
-//
+// Comparison operator
+/*public class Operator {
+    public static void main(String[] args){
+
+        int a =10;
+        int b=20;
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a!=b);
+
+    }
+}*/
+
+// Assignment Operator
+/*public class Operator {
+    public static void main(String[] args){
+        int a =10;
+        int b=20+a; //Assignment Operator
+        System.out.println(b);
+
+
+
+    }
+}
+*/
+//Logical Operator
+/*public class Operator {
+    public static void main(String[] args){
+        int a =10;
+        int b=20;
+        int c=20;
+        System.out.println(b==a&&b==c);
+        System.out.println(b==a || b==c );
+        System.out.println(b!=a);
+
+    }
+}*/
+
+//Bitwise Operator
 public class Operator {
     public static void main(String[] args){
+        int a =10;
+         System.out.println(2&3);
 
     }
 }
