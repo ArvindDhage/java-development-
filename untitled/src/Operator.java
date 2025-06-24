d56926e79797808ce17f46ff8ba5b7cb585a8c6a
Arvind Dhage <arvinddhage35@gmail.com>
2025-06-24 09:22:26 +0530

    Arithmetic operator

---
+++ untitled/src/Operator.java
@@ -0,0 +1,28 @@
+
+//Arithmetic Operator
+/*public class Operator {
+    public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            int f = a / b;
+            int g = a % b;
+
+            System.out.println("Addition of two number is :" + c);
+            System.out.println("Subtraction of two number is :" + d);
+            System.out.println("Multiplication of two number is :" + e);
+            System.out.println("Division of two number is :" + f);
+            System.out.println("Remainder is :" + g);
+        }
+    }*/
+
+
+//
+public class Operator {
+    public static void main(String[] args){
+
+    }
+}
