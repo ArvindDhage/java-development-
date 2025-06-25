aba246986a9af9a8f986d88653f0ad3e574e41e9
Arvind Dhage <arvinddhage35@gmail.com>
2025-06-25 07:15:36 +0530

    Precedance &Associativity

---
+++ untitled/src/AssociativityofOperators.java
@@ -0,0 +1,29 @@
+public class AssociativityofOperators {
+    public static void main(String[] args){
+       /* int a=6*5-34/2;
+        //=30-34/2
+        //=30-17
+        //=13
+        System.out.println(a);
+        //Precedance &Associativity
+
+        int b=60/5-34*2;
+        //=12-34*2
+        //=12-68
+        //-56
+        //Higest Precedence goes to * and / .they are then Evaluated on the basis of left to right associativity .
+        System.out.println(b);
+*/
+        //Quick Quiz
+        int x=6;
+        int y=1;
+        //int k= x-y/2;
+        int b=1;
+        int c=4;
+        int a =5;
+        int k=a*a-(4*a*c)/(2*a);
+        System.out.println(k);
+
+
+    }
+}
