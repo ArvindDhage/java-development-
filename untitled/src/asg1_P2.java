9039ae21fa6bd9dc88cb7a494fe5e26ece2d95f3
Arvind Dhage <arvinddhage35@gmail.com>
2025-07-31 13:48:31 +0530

    Added asg1_p2.java

---
+++ untitled/src/asg1_P2.java
@@ -0,0 +1,12 @@
+// Take a name as a input and print a greeting massage for that Particular name .
+import java.util.Scanner;
+public class asg1_P2 {
+    public static void main(String []args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a name :");
+        String name=input.next();
+        System.out.println("Hello  "+name+"  Have a nice day ");
+    }
+
+    
+}
